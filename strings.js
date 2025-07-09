c9347fddfe7e30b225291dca016b2d8295d7c7b2
Sahitya <114092225+Sahitya000@users.noreply.github.com>
2025-07-09 09:57:23 +0530

    Update strings.js

--- strings.js
+++ strings.js
@@ -1,7 +1,7 @@
 // strings.js
 const strings = {
   logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png",
-  title: "InstaMax Update coming Tommorow!✨",
+  title: "InstaMax Update coming Today, Get read!✨",
   message: "All crashes & bugs will be fixed.<br>Sorry for the inconvenience.<br>Thank you for your patience!",
   buttonText: "Join Telegram",
   buttonLink: "https://t.me/Sktech_000"
