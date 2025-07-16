b9e5307e220092e3f621e6e29854664aaa8a63af
Sahitya <114092225+Sahitya000@users.noreply.github.com>
2025-07-16 19:08:50 +0530

    Update strings.js

--- strings.js
+++ strings.js
@@ -1,8 +1,8 @@
 // strings.js
 const strings = {
   logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png",
-  title: "InstaMax Update released!✨",
-  message: "All crashes & bugs are fixed.<br>Sorry for the inconvenience.<br>Thank you for your patience!<br>This story will automatically disappear after 24 hours!<br>",
+  title: "InstaMax Update coming soon!✨",
+  message: "All crashes & bugs will fixed soon .<br>Sorry for the inconvenience.<br>Thank you for your patience!<br>This story will automatically disappear after 24 hours!<br>",
   buttonText: "Join Telegram",
   buttonLink: "https://t.me/Sktech_000"
 };
